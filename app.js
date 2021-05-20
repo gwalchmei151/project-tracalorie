7c9f9415cf46ff28f0738d4d82d5530ff0b46534
Tushar Ismail <gwalchmei@gmail.com>
2021-05-21 00:31:14 +0800

    started building the controllers

---
+++ app.js
@@ -0,0 +1,47 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function () {
+  // Item Constructor
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+
+  // Data Structure / State(in react)
+  const data = {
+    items: [
+      { id: 0, name: 'Steak Dinner', calories: 1200 },
+      { id: 1, name: 'Cookie', calories: 400 },
+      { id: 2, name: 'Eggs', calories: 300 },
+    ],
+    currentItem: null,
+    totalCalories: 0,
+  };
+  // Public methds
+  return {
+    logData: function () {
+      return data;
+    },
+  };
+})();
+
+// UI Controller
+const UICtrl = (function () {
+  // Public Methods
+  return {};
+})();
+
+// App Controller
+const App = (function (ItemCtrl, UICtrl) {
+  return {
+    init: function () {
+      console.log('Initialising App...');
+    },
+  };
+})(ItemCtrl, UICtrl);
+
+// Initialise App
+
+App.init();
